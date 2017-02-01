2d86829a94d82f172a6084525b818ffc7391580b
0xe06d7363 <swiftsdk@outlook.com>
2017-02-01 16:08:10 +0400

    checknumbers

---
+++ checknext_0102.pas
@@ -0,0 +1,32 @@
+program numbers_0102;
+
+var n, k1, k2, k3, s: integer;
+
+begin
+        S := 0;
+
+        writeln('Введите два числа: ');
+
+        read(k1, k2);
+
+        read(k3);
+
+        repeat
+
+
+                if (k2 > k1) and (k2 > k3) then
+
+                        s := s + 1;
+
+
+                k1 := k2;
+
+                k2 := k3;
+
+                read(k3);
+
+        until k3 = 0;
+
+        writeln('Количетсво цифр: ',S);
+
+end.
